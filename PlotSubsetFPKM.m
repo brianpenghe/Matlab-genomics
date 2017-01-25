2d9b5b77e88d022d4205304843b1fadc603b41a3
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-01-24 21:28:13 -0800

    adjusted display range

--- PlotSubsetFPKM.m
+++ PlotSubsetFPKM.m
@@ -8,5 +8,5 @@ function PlotSubsetFPKM = PlotSubsetFPKM(FPKM,Gname,head)
 A=readtable('test.dat','ReadVariableNames',false);
 B=table2cell(A);
 [C,ia,ib]=intersect(Gname,B);
-clustergram(log2(FPKM(ia,:)+0.1),'Cluster',1,'RowLabels',Gname(ia),'RowPDist','spearman','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+clustergram(log2(FPKM(ia,:)+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',Gname(ia),'RowPDist','spearman','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 end
