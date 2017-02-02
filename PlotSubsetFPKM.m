cd94f610209a12da8591a17f8f4ae5f8de18ffb4
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-02-01 16:08:28 -0800

    added non-cluster option; use Pearson but not Spearman now

--- PlotSubsetFPKM.m
+++ PlotSubsetFPKM.m
@@ -1,12 +1,19 @@
-function PlotSubsetFPKM = PlotSubsetFPKM(FPKM,Gname,head)
+function PlotSubsetFPKM = PlotSubsetFPKM(FPKM,Gname,head,type)
 %PlotSubsetFPK is a function to plot the heatmap of FPKM values of a subset of genes 
 %Useful for investigating the expression patterns of a short list of genes
 %PlotSubsetFPKM = PlotSubsetFPKM(FPKM,GeneName,head)
 %The gene list should be stored in a single column in test.dat in the
 %current directory
+if nargin == 3
+type = 'c';
+end;
 
 A=readtable('test.dat','ReadVariableNames',false);
 B=table2cell(A);
 [C,ia,ib]=intersect(Gname,B);
-clustergram(log2(FPKM(ia,:)+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',Gname(ia),'RowPDist','spearman','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+if type == 'c'
+clustergram(log2(FPKM(ia,:)+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',Gname(ia),'RowPDist','correlation','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+else if type == 'h'
+HeatMap(log2(FPKM(ia,:)+0.1),'DisplayRange',12,'RowLabels',Gname(ia),'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+end
 end
