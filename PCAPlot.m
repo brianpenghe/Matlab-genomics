363dd9afb824016b2c94887e3b254588fe89da1c
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-02-21 23:05:18 -0800

    added a function to generate scatter plot of PCA

---
+++ PCAPlot.m
@@ -0,0 +1,22 @@
+function [COEFF,SCORE,latent] = PCAPlot(Matrix,Head,d)
+%PCAPlot is a function to calculate PCs and plot out an image and .avi video
+%The Matrix is to be transposed if you do Sample PCA for a gene table
+%filtering and log transformation may have to be performed before using this function
+%array d is required for specifying colors
+%array Head is needed for labeling Sample Names
+%This function depends on other functions in the same folder
+%function [COEFF,SCORE,Latent] = PCAPlot(Matrix,Head,d);
+
+
+[COEFF,SCORE,latent] = princomp(Matrix);
+colormap(jet)
+scatter3(SCORE(:,1),SCORE(:,2),SCORE(:,3),50,d,'filled');
+ylabel(strcat('PC2(',strcat(num2str(round(latent(2)/sum(latent)*100)),'%'),')'))
+zlabel(strcat('PC3(',strcat(num2str(round(latent(3)/sum(latent)*100)),'%'),')'))
+xlabel(strcat('PC1(',strcat(num2str(round(latent(1)/sum(latent)*100)),'%'),')'))
+text(SCORE(:,1)+0.5,SCORE(:,2)+0.5,SCORE(:,3)+0.5,Head);
+OptionZ.FrameRate=60;OptionZ.Duration=11;OptionZ.Periodic=true;
+CaptureFigVid([-20,10;-110,10;-190,80;-290,10;-380,10],'testvid.avi',OptionZ)
+
+end
+
