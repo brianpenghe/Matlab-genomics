ae6d6286b97c92739d3c73fc1860306952e5f64c
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-02-21 23:05:33 -0800

    added custom file name feature

--- SaveCell.m
+++ SaveCell.m
@@ -1,8 +1,8 @@
-function SaveCell = SaveCell(cell)
-%SaveCell is a function to write a nX1 cell array into a file named 'test.dat'
+function SaveCell = SaveCell(cell,filename)
+%SaveCell is a function to write a nX1 cell array into a file named filename
 %Useful for saving lists of genes, for example
-%SaveCell(cell_array)
-fileID = fopen('test.dat','w');
+%SaveCell(cell_array,filename)
+fileID = fopen(filename,'w');
 formatSpec='%s\n';
 [sizem, sizen]=size(cell);
 for row=1:sizem
