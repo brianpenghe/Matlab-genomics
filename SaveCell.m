61ded7aed87cdf5372ddce06e6bf2ed8eeeb4f1a
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-02-23 12:03:28 -0800

    simplified SaveCell.m

--- SaveCell.m
+++ SaveCell.m
@@ -2,11 +2,5 @@ function SaveCell = SaveCell(cell,filename)
 %SaveCell is a function to write a nX1 cell array into a file named filename
 %Useful for saving lists of genes, for example
 %SaveCell(cell_array,filename)
-fileID = fopen(filename,'w');
-formatSpec='%s\n';
-[sizem, sizen]=size(cell);
-for row=1:sizem
-    fprintf(fileID,formatSpec,cell{row,1});
-end
-fclose(fileID);
+writetable(cell2table(cell),filename,'WriteVariableNames',false)
 end
