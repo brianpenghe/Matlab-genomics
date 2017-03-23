3b82a840dae614e59e5e5a32184ce4ded00c7a84
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-03-22 23:32:00 -0700

    GOTree now consider missing node cases due to GO database inconsistency

--- GOTreeColor.m
+++ GOTreeColor.m
@@ -7,17 +7,19 @@ function Tree = GOTree(filename,GO)
 %make sure you replace all the GO:XXXX with XXXX
 %Copy paste the original p-value and GO ID columns onto a new file
 
-A=readtable(filename);
-[m n]=size(A);
+A=readtable(filename,'ReadVariableNames',false);
+
 JET=colormap(jet);
 ColorCode=-log10(table2array(A(:,1)));
 ColorNorm=floor(sqrt((ColorCode-min(ColorCode))./(max(ColorCode)-min(ColorCode)+0.1))*64)+1;
 testgo=GO(table2array(A(:,3)));
 cm=getmatrix(testgo);
 Tree=biograph(cm,get(testgo.Terms,'name'));
+NewOrder=GetOrder(Mat2StrArray(cell2mat(get(testgo.Terms,'id'))),Mat2StrArray(table2array(A(:,3))));
+[m n]=size(NewOrder);
 
 for i=1:m
-set(Tree.Nodes(i),'Color',JET(ColorNorm(i),:))
+set(Tree.Nodes(i),'Color',JET(ColorNorm(NewOrder(i)),:))
 end
 
 view(Tree)
