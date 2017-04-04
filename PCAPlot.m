15864798428bae2ffeb9b5fb30a723149f17aaeb
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-04-04 13:55:22 -0700

    fixed video initialization problem by adding “figure;”

--- PCAPlot.m
+++ PCAPlot.m
@@ -6,9 +6,10 @@ function [COEFF,SCORE,latent] = PCAPlot(Matrix,Head,GeneName,d)
 %array Head is needed for labeling Sample Names
 %This function depends on other functions in the same folder
 %function [COEFF,SCORE,Latent] = PCAPlot(Matrix,Head,d);
-
+%This function depends on discretize.m which only works on Matlab R2016b or later
 
 [COEFF,SCORE,latent] = princomp(Matrix);
+figure;
 colormap(jet)
 scatter3(SCORE(:,1),SCORE(:,2),SCORE(:,3),50,d,'filled');
 ylabel(strcat('PC2(',strcat(num2str(round(latent(2)/sum(latent)*100)),'%'),')'))
@@ -19,8 +20,15 @@ OptionZ.FrameRate=60;OptionZ.Duration=11;OptionZ.Periodic=true;
 CaptureFigVid([-20,10;-110,10;-190,80;-290,10;-380,10],'testvid.avi',OptionZ)
 Index=COEFF; %This is just for getting the same dimentionality
 for i=1:20
-[test,Index(:,i)]=sort(COEFF(:,i));
-SaveCell(GeneName(Index(1:100,i)),strcat(num2str(i),'_neg.txt'));SaveCell(GeneName(Index(end:-1:end-99,i)),strcat(num2str(i),'_pos.txt'));
+    [test,Index(:,i)]=sort(COEFF(:,i));
+    SaveCell(GeneName(Index(1:100,i)),strcat(num2str(i),'_neg.txt'));
+    SaveCell(GeneName(Index(end:-1:end-99,i)),strcat(num2str(i),'_pos.txt'));
 end
+
+PlotSet=reshape(Index([1:100 end:-1:end-99],1:3),600,1);
+[Y,E]=discretize(COEFF(:,1),100);
+figure;
+scatter3(COEFF(:,1),COEFF(:,2),COEFF(:,3),20,Y,'filled')
+text(COEFF(PlotSet,1),COEFF(PlotSet,2),COEFF(PlotSet,3),GeneName(PlotSet))
 end
 
