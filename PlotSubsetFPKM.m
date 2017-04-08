8bc04a6c0cdae3cbf2d099df33afa9e325107da9
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-04-07 21:54:55 -0700

    modified two-way clustering code

--- PlotSubsetFPKM.m
+++ PlotSubsetFPKM.m
@@ -14,7 +14,7 @@ B=table2cell(A);
 if type == 'c'
 clustergram(log2(FPKM(ismember(Gname,B),:)+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',Gname(ismember(Gname,B)),'RowPDist','correlation','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'd'
-clustergram(log2(FPKM(ismember(Gname,B),:)+0.1),'DisplayRange',12,'RowLabels',Gname(ismember(Gname,B)),'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+clustergram(log2(FPKM(ismember(Gname,B),:)+0.1),'DisplayRange',12,'RowLabels',Gname(ismember(Gname,B)),'RowPDist','correlation','ColumnLabels',head,'Linkage','complete','Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'h'
 HeatMap(log2(FPKM(ismember(Gname,flipud(B)),:)+0.1),'DisplayRange',12,'RowLabels',Gname(ismember(Gname,flipud(B))),'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'o'
