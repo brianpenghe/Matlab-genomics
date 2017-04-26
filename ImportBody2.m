04def0f58df173af4ee43fe44f2611876eb12365
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-04-26 14:55:44 -0700

    reordered input arguments and optimized cases

--- ImportBody2.m
+++ ImportBody2.m
@@ -1,9 +1,10 @@
-function [name,value,head] = ImportBody(filename,Delimiter)
+function [name,value,head] = ImportBody2(filename,stringNo,Delimiter)
 %ImportBody is a function to import a table from a csv file with header and measurement names
 %The input file should be a complete .csv or other file
 %If no Delimiter specified, we assume it is a csv.
-%function [name,value,head] = ImportBody(filename,Delimiter)
+%function [name,value,head] = ImportBody(filename,Delimiter,stringNo)
 %Delimiter can be c(comma) or t(tab)
+%Make sure you specify how many columns of strings it contains to the left
 
 %File example:
 %Name     ID    HP
@@ -11,8 +12,10 @@ function [name,value,head] = ImportBody(filename,Delimiter)
 %Diane    1322   124
 %Sean     823    76
 
-
-if nargin == 1
+if nargin <2
+stringNo = 1;
+end;
+if nargin < 3 
 Delimiter = 'c';
 end;
 
@@ -24,7 +27,7 @@ end;
 
 DataColumns=varfun(@isnumeric,Table(1,:),'output','uniform');
 head=Table.Properties.VariableNames(DataColumns);
-name=table2cell(Table(2:end,1));
+name=table2cell(Table(2:end,stringNo));
 value=table2array(Table(2:end,DataColumns));
 
 end;
