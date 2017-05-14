556e494f7333e4e95f94c89e10d341bfcc647797
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-05-14 12:26:39 -0700

    [important]simplifying the code and also output the matrix

--- PlotSubsetFPKM.m
+++ PlotSubsetFPKM.m
@@ -1,4 +1,4 @@
-function MyClg = PlotSubsetFPKM(FPKM,Gname,head,type)
+function [MyClg MyMat] = PlotSubsetFPKM(FPKM,Gname,head,type)
 %PlotSubsetFPK is a function to plot the heatmap of FPKM values of a subset of genes 
 %Useful for investigating the expression patterns of a short list of genes
 %PlotSubsetFPKM = PlotSubsetFPKM(FPKM,GeneName,head,type)
@@ -12,15 +12,17 @@ end;
 A=readtable('test.dat','ReadVariableNames',false);
 B=table2cell(A);
 OrderIndex=GetOrder(flipud(B),Gname);
+MyMat = FPKM(OrderIndex(OrderIndex>0),:);
+MyLabels = Gname(OrderIndex(OrderIndex>0));
 if type == 'c'
-MyClg = clustergram(log2(FPKM(ismember(Gname,B),:)+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',Gname(ismember(Gname,B)),'RowPDist','correlation','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+MyClg = clustergram(log2(MyMat+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',MyLabels,'RowPDist','correlation','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'd'
-MyClg = clustergram(log2(FPKM(ismember(Gname,B),:)+0.1),'DisplayRatio',[0.1 0.1],'DisplayRange',12,'RowLabels',Gname(ismember(Gname,B)),'RowPDist','correlation','ColumnLabels',head,'Linkage','complete','Colormap',colormap(jet),'Symmetric','false')
+MyClg = clustergram(log2(MyMat+0.1),'DisplayRatio',[0.1 0.1],'DisplayRange',12,'RowLabels',MyLabels,'RowPDist','correlation','ColumnLabels',head,'Linkage','complete','Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'h'
-MyClg = HeatMap(log2(FPKM(ismember(Gname,flipud(B)),:)+0.1),'DisplayRange',12,'RowLabels',Gname(ismember(Gname,flipud(B))),'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+MyClg = HeatMap(log2(MyMat+0.1),'DisplayRange',12,'RowLabels',MyLabels,'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'o'
-MyClg = HeatMap(log2(FPKM(OrderIndex(OrderIndex>0),:)+0.1),'DisplayRange',12,'RowLabels',Gname(OrderIndex(OrderIndex>0)),'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+MyClg = HeatMap(log2(MyMat+0.1),'DisplayRange',12,'RowLabels',MyLabels,'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'oc'
-MyClg = clustergram(log2(FPKM(OrderIndex(OrderIndex>0),:)+0.1),'OptimalLeafOrder','true','Cluster',2,'Linkage','complete','DisplayRatio',[0.1 0.1],'DisplayRange',12,'RowLabels',Gname(OrderIndex(OrderIndex>0)),'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
+MyClg = clustergram(log2(MyMat+0.1),'OptimalLeafOrder','true','Cluster',2,'Linkage','complete','DisplayRatio',[0.1 0.1],'DisplayRange',12,'RowLabels',MyLabels,'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 end
 
