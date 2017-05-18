c42ddfda8513260504d427d868420f7b22dee96a
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-05-17 21:45:31 -0700

    added distance option for two-way

--- PlotSubsetFPKM.m
+++ PlotSubsetFPKM.m
@@ -1,12 +1,16 @@
-function [MyClg MyMat] = PlotSubsetFPKM(FPKM,Gname,head,type)
+function [MyClg MyMat] = PlotSubsetFPKM(FPKM,Gname,head,type,PDist)
 %PlotSubsetFPK is a function to plot the heatmap of FPKM values of a subset of genes 
 %Useful for investigating the expression patterns of a short list of genes
-%PlotSubsetFPKM = PlotSubsetFPKM(FPKM,GeneName,head,type)
+%PlotSubsetFPKM = PlotSubsetFPKM(FPKM,GeneName,head,type,PDist)
 %The gene list should be stored in a single column in test.dat in the
 %current directory
 %possible types out plots: c(luster) d(ouble cluster) h(eatmap) o(rdered heatmap) o(rdered)c(lustergram)
+%PDist specifies the distance used for clustering cells under d mode
 if nargin == 3
 type = 'c';
+end;
+if nargin == 4
+PDist = 'Euclidiean';
 end;
 
 A=readtable('test.dat','ReadVariableNames',false);
@@ -17,7 +21,7 @@ MyLabels = Gname(OrderIndex(OrderIndex>0));
 if type == 'c'
 MyClg = clustergram(log2(MyMat+0.1),'DisplayRange',12,'Cluster',1,'RowLabels',MyLabels,'RowPDist','correlation','ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'd'
-MyClg = clustergram(log2(MyMat+0.1),'DisplayRatio',[0.1 0.1],'DisplayRange',12,'RowLabels',MyLabels,'RowPDist','correlation','ColumnLabels',head,'Linkage','complete','Colormap',colormap(jet),'Symmetric','false')
+MyClg = clustergram(log2(MyMat+0.1),'DisplayRatio',[0.1 0.1],'DisplayRange',12,'RowLabels',MyLabels,'RowPDist','correlation','ColumnPDist',PDist,'ColumnLabels',head,'Linkage','complete','Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'h'
 MyClg = HeatMap(log2(MyMat+0.1),'DisplayRange',12,'RowLabels',MyLabels,'ColumnLabels',head,'Colormap',colormap(jet),'Symmetric','false')
 elseif type == 'o'
