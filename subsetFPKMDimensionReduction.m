9946f32717ece8179f1c63b871f56c244b1b6992
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-06-05 16:08:45 -0700

    added a script for multiple dimension reduction methods

---
+++ subsetFPKMDimensionReduction.m
@@ -0,0 +1,22 @@
+function [MyClg MyMat MyLabels PCAOutput tSNEOutput] = subsetFPKMDimensionReduction(FPKM,Gname,head,type,PDist,Standardize)
+%PlotSubsetFPKM, PCAPlot and tsnePlot are called together 
+%Useful for quickly reduce FPKM matrix dimensionality
+%[MyClg MyMat MyLabels PCAOutput tSNEOutput] = subsetFPKMDimensionReduction(FPKM,Gname,head,type,PDist,Standardize)
+%The gene list should be stored in a single column in test.dat in the current directory
+
+if nargin < 4
+type = 'c';
+end;
+if nargin < 5
+PDist = 'euclidean';
+end;
+if nargin < 6
+Standardize = 0;
+end;
+
+[MyClg MyMat MyLabels]=PlotSubsetFPKM(FPKM,Gname,head,type,PDist,Standardize);
+PCAOutput=PCAPlot(log2(MyMat+0.1),head,MyLabels,'black',100);
+tSNEOutput=tsnePlot(log2(MyMat+0.1),'black',50,2,Head);
+
+end
+
