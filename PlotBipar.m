570a57084f9906b441770ca003118dfa5bd9d345
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-06-05 11:56:45 -0700

    [important]updated colors, added lower bound

--- PlotBipar.m
+++ PlotBipar.m
@@ -1,7 +1,7 @@
-function biog = PlotBipar(Filename,Cap,TransformMethod)
+function biog = PlotBipar(Filename,Cap,TransformMethod,Watershed)
 %PlotBipar plots a bipartite graph based on the matrix stored in the file Filename
 %The matrix in the Filaname is a logical matrix where Cij~=0 indicates i -> j. Cij is the weight of the edge
-%RowNames and VariableNames have to be included in the Filaname
+%RowNames and VariableNames have to be included in the Filename
 %It also converts the matrix into a square one
 % Old matrix               New Matrix
 %                          -------
@@ -12,8 +12,10 @@ function biog = PlotBipar(Filename,Cap,TransformMethod)
 %                          *#*%^--
 %                          #^%!#--
 %
-%TransformMethod is optional. It can be 'l'(og) or 's'(qrt)
+%TransformMethod is optional. It can be 'l'(og) or 's'(qrt) 
 %Censor ultrahigh values to Cap
+%Turn small values lower than Watershed to zero
+%If nargin==1, weighting is ignored
 
 CMatrix=readtable(Filename,'ReadRowNames',true,'ReadVariableNames',true);
 Name1=CMatrix.Properties.RowNames;
@@ -24,39 +26,44 @@ Name2=CMatrix.Properties.VariableNames';
 CMatrix2=repmat([0],m1+m2);
 CMatrix2(1:m1,end-m2+1:end)=table2array(CMatrix);
 
+if nargin>3
+CMatrix2(CMatrix2<Watershed)=0;
+end;
+
 CMatrix3=CMatrix2;
 CMatrix3(CMatrix3>0)=1;
+
 biog=biograph(CMatrix3,[Name1;Name2]','LayoutType','radial');
 for i=1:m1
-    set(biog.Nodes(i),'Color',[1,0.5,0.5])
+    set(biog.Nodes(i),'Color',[1,0.7,0.7])
 end
 for i=m1+1:m1+m2
     set(biog.Nodes(i),'Color',[0.7,0.7,1])
     set(biog.Nodes(i),'Shape','Circle')
 end
 
-Weights=reshape(CMatrix2,[],1);
-Weights(Weights==0)=[];
-              
-if nargin<3
-    'No Transformation'
-else
-    if TransformMethod=='l'
-        Weights=log10(Weights);
-    elseif TransformMethod=='s'
-        Weights=sqrt(Weights);
-    end;
-end;
-
 if nargin>1
+              
+    Weights=reshape(CMatrix2,[],1);
+    Weights(Weights==0)=[];
+              
+    if nargin<3
+        'No Transformation'
+    else
+        if TransformMethod=='l'
+            Weights=log10(Weights);
+        elseif TransformMethod=='s'
+            Weights=sqrt(Weights);
+        end;
+    end;
+
     Weights(Weights>Cap)=Cap;
-end;
               
-Weights=Weights./max(Weights)*2;
-[m n] = size(Weights);
-for i=1:m
-    biog.Edges(i).LineWidth=Weights(i);
-end;
-              
+    Weights=Weights./max(Weights)*2;
+    [m n] = size(Weights);
+    for i=1:m
+        biog.Edges(i).LineWidth=Weights(i);
+    end;
+end;              
 view(biog)
 end
