eecad5c5779f14dd946ece81adda9fc617bf6a9f
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-06-14 13:49:50 -0700

    added a script to convert table to DataMatrix for clustering

---
+++ Table2DataMatrix.m
@@ -0,0 +1,10 @@
+function NewDataMatrix = Table2DataMatrix(OldTable)
+%This script converts a table to DataMatrix
+%The first column of the table should contain the ID/RowLabels
+%VariableNames will be used as ColumnLabels
+%Useful for clustergram / heatmap analysis
+
+import bioma.data.DataMatrix;
+NewDataMatrix=DataMatrix(table2array(OldTable(:,2:end)),table2array(OldTable(:,1)),OldTable(:,2:end).Properties.VariableNames);
+
+end
