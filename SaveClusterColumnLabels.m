f21bc33917853228b1344a1d58ab512b364bcb3d
brianpenghe@gmail.com <brianpenghe@gmail.com>
2017-08-08 15:53:13 -0700

    added a script to save cluster column labels

---
+++ SaveClusterColumnLabels.m
@@ -0,0 +1,10 @@
+function SaveClusterColumnLabels = SaveClusterColumnLabels(Cluster)
+%SaveClusterColumnLabels is a function to write the ColumnLabels of a 1x1
+%clustergram into a file named as the clustergram
+%Useful for saving ColumnLabels of subclusters
+%SaveClusterColumnLabels(Cluster)
+
+s=inputname(1);
+ColumnLabels=flipud(get(Cluster,'ColumnLabels'));
+SaveCell(ColumnLabels,s);
+end
