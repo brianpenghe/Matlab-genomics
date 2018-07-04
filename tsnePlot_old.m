65aeecd6f6af163e0f8f4b608cda29dde406d195
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-07-03 17:10:20 -0700

    updated tsne name clash

--- tsnePlot_old.m
+++ tsnePlot_old.m
@@ -1,17 +1,17 @@
-function mappedX = tsnePlot(Matrix,d,sc,DimNo,Head)
+function mappedX = tsnePlot_old(Matrix,d,sc,DimNo,Head)
 %The input matrix is a gene X sample matrix
 %             Sample1 Sample2 Sample3
 %      gene1
 %      gene2
 %      gene3
-%tsnePlot is a function to reduce the dimension of columns
+%tsnePlot_old is a function to reduce the dimension of columns before Matlab 2018a
 %The Matrix is to be transposed if you do Sample tsne for a gene table
 %filtering and log transformation may have to be performed before using this function
 %array d is required for specifying colors
 %array sc is optional for specifying sizes, default is all 50
 %array Head is needed for labeling Sample Names
 %This function depends on other functions in the same folder
-%function mappedX = tsnePlot(Matrix,d,sc,DimNo,Head);
+%function mappedX = tsnePlot_old(Matrix,d,sc,DimNo,Head);
 %DimNo is the number of dimensions you want. Default is 2 but you can set it to 3
 
 if nargin < 3
@@ -21,7 +21,7 @@ if nargin < 4
     DimNo=2
 end;
 
-mappedX = tsne(Matrix',[], DimNo, 30, 30);
+mappedX = tsne_old(Matrix',[], DimNo, 30, 30);
 figure;
 colormap(jet)
 if DimNo == 2
