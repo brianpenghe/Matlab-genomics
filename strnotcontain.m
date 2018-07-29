c4088915e2631bfd2819a4c80afea0cea72ce25d
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-07-29 12:29:38 -0700

    updated comments

--- strnotcontain.m
+++ strnotcontain.m
@@ -1,5 +1,6 @@
-function Index = strcontain(str,strarray)
-%Similar to strmatch, this also prints out the indices.
+function Index = strnotcontain(str,strarray)
+%opposite to strcontain
+%Similar to strmatch, strcontain also prints out the indices.
 %But strcontain only requires the str to be contained as substrings, not necessarily as prefix
 
 Index=find(cellfun(@isempty,strfind(strarray,str)));
