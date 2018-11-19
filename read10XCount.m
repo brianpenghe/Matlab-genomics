7d7d2ab50679c9e9a15c947981248d11a8995049
brianpenghe@gmail.com <brianpenghe@gmail.com>
2018-11-18 20:32:19 -0800

    added codes for CellRanger Default

--- read10XCount.m
+++ read10XCount.m
@@ -2,6 +2,12 @@ function Matrix_dm=read10XCount(MtxFile,GeneFile,CellFile)
 %read10XCount is a function to import 10X single-cell RNA-seq UMI counts as MatrixMarket formats and converts it into a DataMatrix format on Matlab 
 %Matrix_dm = read10XCount(MtxFile,GeneFile,CellFile)
 %the input arguments are paths or names of the input files
+if nargin < 1
+    MtxFile='matrix.mtx';
+    GeneFile='genes.tsv';
+    CellFile='barcodes.tsv';
+end
+
 copyfile(MtxFile,strcat(MtxFile,'.txt'));
 copyfile(GeneFile,strcat(GeneFile,'.txt'));
 copyfile(CellFile,strcat(CellFile,'.txt'));
@@ -14,5 +20,6 @@ Genes_table2=array2table(strcat(Genes_array(:,1),'_',Genes_array(:,2)));
 Matrix_table=[Genes_table2(1:height(array2table(Matrix_matrix)),:) array2table(Matrix_matrix)];
 Matrix_table.Properties.VariableNames=[{'Var1'} ValidizeNames(table2array(Cells_table)')];
 Matrix_dm=Table2DataMatrix(Matrix_table);
+
 end
 
