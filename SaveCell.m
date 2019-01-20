7e21889ad51c29763b86323402979081cd24c43e
brianpenghe@gmail.com <brianpenghe@gmail.com>
2019-01-19 20:11:34 -0800

    added delimiter flexibility to SaveCell

--- SaveCell.m
+++ SaveCell.m
@@ -1,6 +1,10 @@
-function SaveCell = SaveCell(cell,filename)
+function SaveCell = SaveCell(cell,filename,dlm)
 %SaveCell is a function to write a nX1 cell array into a file named filename
 %Useful for saving lists of genes, for example
 %SaveCell(cell_array,filename)
-writetable(cell2table(cell),filename,'WriteVariableNames',false)
+%SaveCell(cell_array,filename,'\t')
+if nargin < 3
+dlm=',';
+end
+writetable(cell2table(cell),filename,'WriteVariableNames',false,'Delimiter',dlm)
 end
