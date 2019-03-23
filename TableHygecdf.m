b03a4421af998c1bb1deed8388d00e5ea5f4c69c
brianpenghe@gmail.com <brianpenghe@gmail.com>
2019-03-23 16:14:57 +0000

    [important]added a new script to use a contingency table as input for hygecdf

---
+++ TableHygecdf.m
@@ -0,0 +1,60 @@
+function [P Tail]= MetaHygecdf(I)
+%TableHygecdf makes use of the Intersect matrix to calculate hypergeometric enrichment cdf. The table is a contingency table. Rows and Columns are two different set of mutually exclusive catogaries. The category labels are VariableNames and RowNames of the table variable.
+%Useful for enrichment analysis
+%Icount are intersect counts 
+%                 **B***D*i*m*e*n*s*i*o*n**
+%                
+%               * *************************
+%               A *                       *
+%               * *      I                *
+%               D *                       *
+%               i *                       *
+%               m *                       *
+%               * *************************
+%The outputs are a matrix of p values and another of upper tails
+%M is a constant representing total population size
+%The x, M, k, N are explained in Matlab readme of hypecdf.m
+
+%Sometimes your heatmap may have 0 count sitting at red boxes. This may be caused by the fact that one of the input lists is too small. Remember Matlab defines cdf(X=0)=pmf(X=0) instead of 0.
+
+[An Bn]=size(I);
+
+A = sum(table2array(I)');
+B = sum(table2array(I));
+M = sum(A); 
+for i=1:An
+    for j=1:Bn
+        P(i, j)=hygecdf(Icount{i,j},M,A(i),B(j));
+        Tail(i, j)=hygecdf(Icount(i,j),M,A(i),B(j),'upper');
+    end
+end
+
+figure
+colormap(jet)
+imagesc(max(-log10(P*An*Bn),0));
+
+for i = 1:An
+    for j = 1:Bn
+    textHandles(j,i) = text(j,i,num2str(I(i,j)),...
+                        'horizontalAlignment','center');
+    end
+end
+set(gca,'XTick',[1:Bn],'XTickLabel',strrep(I.Properties.VariableNames,'_','\_'),'XTickLabelRotation',90)
+set(gca,'YTick',[1:An],'YTickLabel',strrep(I.Properties.RowNames,'_','\_'))
+
+
+figure
+colormap(pink)
+imagesc(max(-log10(Tail*An*Bn),0))
+% add count labels
+
+for i = 1:An
+    for j = 1:Bn
+    textHandles(j,i) = text(j,i,num2str(I(i,j)),...
+                        'horizontalAlignment','center');
+    end
+end
+set(gca,'XTick',[1:Bn],'XTickLabel',strrep(B.Properties.VariableNames,'_','\_'),'XTickLabelRotation',90)
+set(gca,'YTick',[1:An],'YTickLabel',strrep(A.Properties.VariableNames,'_','\_'))
+
+end
