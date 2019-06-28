607a9a3ef7de78f7f94057e81875d8846dcd2970
brianpenghe@gmail.com <brianpenghe@gmail.com>
2019-06-28 10:18:09 +0100

    modified indentation

--- MarkerNormality190628.m
+++ MarkerNormality190628.m
@@ -2,7 +2,7 @@ function P=MarkerNormality190628(Matrix,Correct)
 %MarkerNormality190628 is a function to get P values of ks test to see whether Matrix contains rows that follow a standard normal distribution
 %P=MarkerNormality190628(Matrix,1)
 if nargin < 2
-Correct=0
+    Correct=0
 end
 
 P=[];
@@ -12,14 +12,14 @@ for i=1:size(Matrix,1)
 end
 figure;
 if Correct==1
-Pc=min(1,P.*size(Matrix,1));
-cdfplot(-log10(Pc))
-P=Pc;
-xlabel('-log10(Pcorrected)')
+    Pc=min(1,P.*size(Matrix,1));
+    cdfplot(-log10(Pc))
+    P=Pc;
+    xlabel('-log10(Pcorrected)')
 else
-cdfplot(-log10(P))
-line([-log10(0.05/size(Matrix,1)),-log10(0.05/size(Matrix,1))],[0,1])
-xlabel('-log10(P)')
+    cdfplot(-log10(P))
+    line([-log10(0.05/size(Matrix,1)),-log10(0.05/size(Matrix,1))],[0,1])
+    xlabel('-log10(P)')
 end
 line([-log10(0.05),-log10(0.05)],[0,1])
 ylabel('fraction')
