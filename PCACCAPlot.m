8f2b9905e80836a8c51ff89cc13e9fb37516365a
brianpenghe <brianpenghe@gmail.com>
2019-10-31 09:47:50 +0000

    [important] new feature for PCs
    now can select any PCs for CCA

--- PCACCAPlot.m
+++ PCACCAPlot.m
@@ -1,4 +1,4 @@
-function [COEFF,SCORE,latent,ACOEFF,BCOEFF,rCORR,USCORE,VSCORE,stats,UltiCOEFF] = PCACCAPlot(Matrix,MatrixB,Head,GeneName,MetaName,d,N,dim)
+function [COEFF,SCORE,latent,ACOEFF,BCOEFF,rCORR,USCORE,VSCORE,stats,UltiCOEFF] = PCACCAPlot(Matrix,MatrixB,Head,GeneName,MetaName,d,N,dim,PCs)
 %The input matrix is a gene X sample matrix
 %             Sample1 Sample2 Sample3
 %      gene1
@@ -14,13 +14,17 @@ function [COEFF,SCORE,latent,ACOEFF,BCOEFF,rCORR,USCORE,VSCORE,stats,UltiCOEFF] 
 %function [COEFF,SCORE,latent,ACOEFF,BCOEFF,rCORR,USCORE,VSCORE,stats,UltiCOEFF] = PCACCAPlot(Matrix,MatrixB,Head,GeneName,MetaName,d,N)
 %This function depends on discretize.m which only works on Matlab R2016b or later
 %dim is the number of PCs interested
+if nargin < 9
+    PCs = 1:dim;
+end
+
 figure;
 [COEFF,SCORE,latent] = PCAPlot(Matrix,Head,GeneName,d,N,dim);
-[ACOEFF,BCOEFF,rCORR,USCORE,VSCORE,stats] = CCAPlot(SCORE(:,1:dim)',MatrixB,Head,Mat2StrArray([1:dim]'),MetaName,d,N,dim);
+[ACOEFF,BCOEFF,rCORR,USCORE,VSCORE,stats] = CCAPlot(SCORE(:,PCs)',MatrixB,Head,Mat2StrArray(PCs'),MetaName,d,N,dim);
 [Am An]=size(ACOEFF);
 [Bm Bn]=size(BCOEFF);
 iend=min(dim,min(An,Bn));
-UltiCOEFF=COEFF(:,1:dim)*ACOEFF;
+UltiCOEFF=COEFF(:,PCs)*ACOEFF;
 AIndex=UltiCOEFF;
 for i=1:iend
 [testA,AIndex(:,i)]=sort(UltiCOEFF(:,i));
